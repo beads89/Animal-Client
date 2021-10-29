3cc2b395a4201e4c6534c8e123c4054f3058b26f
Brandon Eads <brandon181989@hotmail.com>
2021-10-29 14:04:57 -0700

    create initial dotnet mvc and model/controller files

---
+++ AnimalClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AnimalClient.Solution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
