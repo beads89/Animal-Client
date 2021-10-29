d99a932b6b7f23f950009c30ba50b79bc8da519a
Brandon Eads <brandon181989@hotmail.com>
2021-10-29 14:24:47 -0700

    add cat/apihelper models code

---
+++ AnimalClient/Models/ApiHelper.cs
@@ -0,0 +1,50 @@
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace AnimalClient.Models
+{
+  class ApiHelper
+    {
+    public static async Task<string> GetAll()
+    {
+      RestClient client = new RestClient("http://localhost:5001/api");
+      RestRequest request = new RestRequest($"cats", Method.GET);
+      var response = await client.ExecuteAsync(request);
+      return response.Content;
+    }
+
+    public static async Task<string> Get(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5001/api");
+      RestRequest request = new RestRequest($"catss/{id}", Method.GET);
+      var response = await client.ExecuteAsync(request);
+      return response.Content;
+    }
+
+    public static async Task Post(string newCat)
+    {
+      RestClient client = new RestClient("http://localhost:5001/api");
+      RestRequest request = new RestRequest($"cats", Method.POST);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newCat);
+      var response = await client.ExecuteAsync(request);
+    }
+
+    public static async Task Put(int id, string newCat)
+    {
+      RestClient client = new RestClient("http://localhost:5001/api");
+      RestRequest request = new RestRequest($"cats/{id}", Method.PUT);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newCat);
+      var response = await client.ExecuteAsync(request);
+    }
+
+    public static async Task Delete(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5001/api");
+      RestRequest request = new RestRequest($"cats/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      var response = await client.ExecuteAsync(request);
+    }
+  }
+}
---
+++ AnimalClient/Models/Cat.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AnimalClient.Models
+{
+    public class Cat
+  {
+    public int CatId { get; set; }
+    public string CatName { get; set; }
+    public string CatGender { get; set; }
+    public string CatBreed { get; set; }
+    public string CatColor { get; set; }
+    public static List<Cat> GetCats()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+
+      List<Cat> catList = JsonConvert.DeserializeObject<List<Cat>>(jsonResponse.ToString());
+
+      return catList;
+    }
+    public static Cat GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Cat cat = JsonConvert.DeserializeObject<Cat>(jsonResponse.ToString());
+
+      return cat;
+    }
+    public static void Post(Cat cat)
+    {
+      string jsonCat = JsonConvert.SerializeObject(cat);
+      var apiCallTask = ApiHelper.Post(jsonCat);
+    }
+
+    public static void Put(Cat cat)
+    {
+      string jsonCat = JsonConvert.SerializeObject(cat);
+      var apiCallTask = ApiHelper.Put(cat.CatId, jsonCat);
+    }
+
+    public static void Delete(int id)
+    {
+      var apiCallTask = ApiHelper.Delete(id);
+    }
+  }
+}
